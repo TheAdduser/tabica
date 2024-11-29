a4553aa14fca840c689c7f3515feb27176e1e94c
TheAdduser <EasyRider190@gmail.com>
2024-11-29 23:36:08 +0100

    init commit

--- tabica/src/App.vue
+++ tabica/src/App.vue
@@ -1,16 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import loginPage from './components/loginPage.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-    
-    </a>
-    <a class="bg-black" href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <loginPage msg="cokolwiek"/>
 </template>
-
---
+++ tabica/src/components/loginPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <button class="bg-gray-700" @click="count++">
+   {{ msg }} {{ count }}
+  </button>
+</template>
+
+
