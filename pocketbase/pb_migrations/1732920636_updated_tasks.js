c6229cc49759d4032980427eef2b554e4c99b63c
TheAdduser <EasyRider190@gmail.com>
2024-11-29 23:57:20 +0100

    add authentication test

---
+++ pocketbase/pb_migrations/1732920636_updated_tasks.js
@@ -0,0 +1,29 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1843675174",
+    "max": 0,
+    "min": 0,
+    "name": "description",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // remove field
+  collection.fields.removeById("text1843675174")
+
+  return app.save(collection)
+})
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import loginPage from './components/loginPage.vue'
+import PocketBase from 'pocketbase';
+import { ref } from 'vue';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+let isLoggedIn = ref(pb.authStore.isValid);
+console.log(pb.authStore.isValid);
 </script>
 
 <template>
-  <loginPage msg="cokolwiek"/>
+  <loginPage msg={{isLoggedIn}} />
 </template>
